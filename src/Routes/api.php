c82caa83dfddfc683f57d2a17678f21d3f96c2f3
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2023-04-16 09:53:43 +0330

    [FIX] methods

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Werify\IdLaravel\Http\Controllers\Api\V1\AuthController;
 
 Route::group(['prefix' => config('werify-auth-service.routes.group')], function () {
-	Route::get(config('werify-auth-service.routes.request-otp'), [config('werify-auth-service.controllers.AuthController.class'), config('werify-auth-service.controllers.AuthController.request-otp')]);
-	Route::get(config('werify-auth-service.routes.verify-otp'), [config('werify-auth-service.controllers.AuthController.class'), config('werify-auth-service.controllers.AuthController.verify-otp')]);
+	Route::post(config('werify-auth-service.routes.request-otp'), [config('werify-auth-service.controllers.AuthController.class'), config('werify-auth-service.controllers.AuthController.request-otp')]);
+	Route::post(config('werify-auth-service.routes.verify-otp'), [config('werify-auth-service.controllers.AuthController.class'), config('werify-auth-service.controllers.AuthController.verify-otp')]);
 });

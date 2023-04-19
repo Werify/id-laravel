f88182c83412531dcd2a7a59f27af2490e3b134e
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2023-04-19 19:29:03 +0330

    [FIX] user profile response

--- src/Jobs/GetUserProfileJob.php
+++ src/Jobs/GetUserProfileJob.php
@@ -15,7 +15,7 @@ class GetUserProfileJob extends BaseJob
 		$path = $this->generateUrl(config('werify-auth-service.api.profile'));
 		$request = $this->get($path, $this->token);
 		if ($request->status() === 200) {
-			return $request->json();
+			return $request->json()['results'] ?? $request->json();
 		}
 		throw new Exception('Failed to get profile');
 	}
